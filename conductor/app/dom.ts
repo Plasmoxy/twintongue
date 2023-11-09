babb49fdc08b5d9733134aded9e27c95427942d5
Plasmoxy <plasmoxy@gmail.com>
2023-11-09 19:45:00 +0100

    bacc

--- conductor/app/dom.ts
+++ conductor/app/dom.ts
@@ -1,21 +1,26 @@
+'use server';
+import { JSDOM } from 'jsdom';
 import type { LrtLyric } from './conductor';
 
 export async function parseLrtLyrics(html: string): Promise<LrtLyric[]> {
-    const el = document.createElement('html');
-    el.innerHTML = html;
+    const dom = new JSDOM(html);
 
     const title: LrtLyric = {
         id: 'title',
-        jp: el.querySelector('#song-title > h1').textContent.trim(),
-        en: el.querySelector('#translation-title > h1').textContent.trim()
+        jp: dom.window.document
+            .querySelector('#song-title > h1')
+            .textContent.trim(),
+        en: dom.window.document
+            .querySelector('#translation-title > h1')
+            .textContent.trim()
     };
 
-    const lyricsDivs = Array.from(el.querySelectorAll('#song-body .par > div'));
-
-    const lyrics: LrtLyric[] = lyricsDivs.map((lyricDiv: Element) => ({
+    const lyrics: LrtLyric[] = [
+        ...dom.window.document.querySelectorAll('#song-body .par > div')
+    ].map((lyricDiv) => ({
         id: lyricDiv.classList[0],
         jp: lyricDiv.textContent.trim(),
-        en: el
+        en: dom.window.document
             .querySelector(`#translation-body .${lyricDiv.classList[0]}`)
             ?.textContent?.trim()
     }));
