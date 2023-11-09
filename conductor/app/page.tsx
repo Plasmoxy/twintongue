73679d0d0972549806ee77bc0d46c892999a2b57
Plasmoxy <plasmoxy@gmail.com>
2023-11-09 19:43:46 +0100

    ach

--- conductor/app/page.tsx
+++ conductor/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { toHiragana } from 'wanakana';
 import { AnalysedToken, analysis, initJmdict } from './conductor';
 import { huePallete } from './hue-colors';
 
 const rljp = `
 もう一回、もう一回。
 「私は今日も転がります。」と、
-少女は言う　少女は言う
+少女は言う少女は言う
 言葉に意味を奏でながら！`;
 const rlen = `One more time, one more time.
 "I'll roll today too."
@@ -17,15 +18,16 @@ export default function Home() {
     const [info, setInfo] = useState<string>('');
     const [jpText, setJpText] = useState(rljp);
     const [enText, setEnText] = useState(rlen);
+    const [lineByLine, setLineByLine] = useState<boolean>(false);
 
-    const [analysed, setAnalysed] = useState<AnalysedToken[]>([]);
+    const [analysed, setAnalysed] = useState<(AnalysedToken | 'sep')[]>([]);
 
     useEffect(() => {
         (async () => {
             setInfo('Loading jmdict');
-            const dict = await fetch('/jmdict-eng-3.5.0.json').then((r) =>
-                r.json()
-            );
+            const dict = await fetch('/jmdict-eng-3.5.0.json', {
+                cache: 'force-cache'
+            }).then((r) => r.json());
             await initJmdict(dict);
             setInfo('Dict loaded, ready to go.');
         })().catch((e) => {
@@ -36,10 +38,25 @@ export default function Home() {
 
     useEffect(() => {
         (async () => {
-            const result = await analysis(jpText, enText);
+            let result: (AnalysedToken | 'sep')[];
+
+            if (lineByLine) {
+                // by jp lines
+                result = [];
+                const jpLines = jpText.split('\n');
+                const enLines = enText.split('\n');
+                for (let i = 0; i < jpLines.length; i++) {
+                    const line = await analysis(jpLines[i], enLines[i]);
+                    result.push(...line);
+                    result.push('sep');
+                }
+            } else {
+                // by entire text
+                result = await analysis(jpText, enText);
+            }
             setAnalysed(result);
         })().catch((e) => console.log(e));
-    }, [jpText, enText]);
+    }, [jpText, enText, lineByLine]);
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -52,6 +69,19 @@ export default function Home() {
                 <p className="text-teal-100">{info}</p>
                 <br />
 
+                <label className="relative inline-flex items-center cursor-pointer">
+                    <input
+                        type="checkbox"
+                        checked={lineByLine}
+                        onChange={(e) => setLineByLine(e.target.checked)}
+                        className="sr-only peer"
+                    />
+                    <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        Analyze line by line
+                    </span>
+                </label>
+
                 <div className="my-10">
                     <label
                         htmlFor="small-input"
@@ -60,7 +90,7 @@ export default function Home() {
                         JP
                     </label>
                     <textarea
-                        className="block w-full p-2 text-gray-900 border border-rose-100 bg-black text-white rounded-lg min-h-[100px] text-[20px]"
+                        className="block w-full p-2 text-gray-900 border border-rose-100 bg-black text-white rounded-lg min-h-[150px] text-[20px]"
                         value={jpText}
                         onChange={(e) => setJpText(e.target.value)}
                     />
@@ -74,27 +104,44 @@ export default function Home() {
                         EN
                     </label>
                     <textarea
-                        className="block w-full p-2 text-gray-900 border border-rose-100 bg-black text-white rounded-lg min-h-[100px]"
+                        className="block w-full p-2 text-gray-900 border border-rose-100 bg-black text-white rounded-lg min-h-[150px]"
                         value={enText}
                         onChange={(e) => setEnText(e.target.value)}
                     />
                 </div>
 
-                <div className="my-10 text-[30px] font-light text-white leading-[100px]">
-                    {analysed.map((token, idx) => (
-                        <ruby>
-                            <span
-                                style={{
-                                    color: huePallete[idx % huePallete.length]
-                                }}
-                            >
-                                {token.text}
-                            </span>
-                            <rt style={{ color: '#d9d9d9' }}>
-                                <span>{token.eng}</span>
-                            </rt>
-                        </ruby>
-                    ))}
+                <div className="my-10 text-[34px] font-light text-white leading-[100px]">
+                    {analysed.map((token, idx) =>
+                        token === 'sep' ? (
+                            <br />
+                        ) : (
+                            <ruby key={`${idx}${token.text}`}>
+                                <ruby>
+                                    <span
+                                        style={{
+                                            color: huePallete[
+                                                idx % huePallete.length
+                                            ]
+                                        }}
+                                    >
+                                        {token.text}
+                                    </span>
+                                    <rt style={{ color: '#d9d9d9' }}>
+                                        {toHiragana(token.token.reading)}
+                                    </rt>
+                                </ruby>
+                                <rt
+                                    style={{
+                                        color: huePallete[
+                                            idx % huePallete.length
+                                        ]
+                                    }}
+                                >
+                                    <span>{token.eng}</span>
+                                </rt>
+                            </ruby>
+                        )
+                    )}
                 </div>
             </div>
         </main>
